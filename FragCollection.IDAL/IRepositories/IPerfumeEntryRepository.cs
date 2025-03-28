6b77a7f88ac7f097cac726295450c4d61acec6ba
Przemek <34289616+przemekbok@users.noreply.github.com>
2025-03-28 21:01:07 +0100

    Update IPerfumeEntryRepository to work with user IDs instead of collection IDs

--- FragCollection.IDAL/IRepositories/IPerfumeEntryRepository.cs
+++ FragCollection.IDAL/IRepositories/IPerfumeEntryRepository.cs
@@ -4,7 +4,8 @@ namespace FragCollection.IDAL.IRepositories
 {
     public interface IPerfumeEntryRepository : IRepository<PerfumeEntry>
     {
-        Task<IEnumerable<PerfumeEntry>> GetByCollectionIdAsync(Guid collectionId);
+        Task<IEnumerable<PerfumeEntry>> GetByUserIdAsync(Guid userId);
+        Task<IEnumerable<PerfumeEntry>> GetPublicEntriesByUserIdAsync(Guid userId);
         Task<IEnumerable<PerfumeEntry>> GetPublicEntriesAsync(int page = 1, int pageSize = 10);
         Task UpdateVolumeAsync(Guid id, decimal newVolume);
     }
