82e06037c7b1151562289bdeb15344a05908e9ec
Przemek <34289616+przemekbok@users.noreply.github.com>
2025-03-28 21:47:36 +0100

    Move PerfumeNoteResponse to DTO project

---
+++ FragCollection.DTO/Perfume/PerfumeNoteResponse.cs
@@ -0,0 +1,18 @@
+using FragCollection.Core.Models;
+
+namespace FragCollection.DTO.Perfume
+{
+    public class PerfumeNoteResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public NoteType Type { get; set; }
+
+        public PerfumeNoteResponse(PerfumeNote note)
+        {
+            Id = note.Id;
+            Name = note.Name;
+            Type = note.Type;
+        }
+    }
+}
