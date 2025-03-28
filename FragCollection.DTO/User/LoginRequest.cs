d9537db2b1bad695e10f8c8cb9fc52630bd58d64
Przemek <34289616+przemekbok@users.noreply.github.com>
2025-03-28 21:47:06 +0100

    Move LoginRequest to DTO project

---
+++ FragCollection.DTO/User/LoginRequest.cs
@@ -0,0 +1,9 @@
+namespace FragCollection.DTO.User
+{
+    public class LoginRequest
+    {
+        public string UsernameOrEmail { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public bool RememberMe { get; set; } = false;
+    }
+}
