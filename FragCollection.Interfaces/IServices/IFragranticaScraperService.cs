9f0c088a81aedd0903996e4e4fd13b247c7b253a
Bokiej <bokiejp1@emea.roche.com>
2025-03-28 16:12:28 +0100

    removal of duplicates

--- FragCollection.Interfaces/IServices/IFragranticaScraperService.cs
+++
@@ -1,9 +0,0 @@
-using FragCollection.Core.Models;
-
-namespace FragCollection.Interfaces.IServices
-{
-    public interface IFragranticaScraperService
-    {
-        Task<PerfumeInfo> ScrapeFragranticaUrlAsync(string url);
-    }
-}
