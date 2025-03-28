8ac671ee8a190232fe95bd3c5b26f89d44f82252
Przemek <34289616+przemekbok@users.noreply.github.com>
2025-03-28 21:01:25 +0100

    Update IUserService to include collection management

--- FragCollection.IServices/IUserService.cs
+++ FragCollection.IServices/IUserService.cs
@@ -5,8 +5,11 @@ namespace FragCollection.IServices
     public interface IUserService
     {
         Task<User?> GetUserByIdAsync(Guid id);
+        Task<User?> GetUserByUsernameAsync(string username);
         Task<User?> AuthenticateAsync(string usernameOrEmail, string password);
         Task<User> RegisterAsync(string username, string email, string password);
         Task UpdateUserAsync(User user);
+        Task UpdateUserCollectionInfoAsync(Guid userId, string name, string description);
+        Task<IEnumerable<User>> GetUsersWithPublicEntriesAsync(int page = 1, int pageSize = 10);
     }
 }
